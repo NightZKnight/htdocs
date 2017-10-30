44979835cb5b953ff06cefc1216d4ed9a10ed1d5
Timothy <32756638+NightZKnight@users.noreply.github.com>
2017-10-30 10:39:38 -0700

    added about page

---
+++ about.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="master.css">
+    <title>Die Mr. Die | Home</title>
+  </head>
+  <body>
+
+    <?php include 'inc/header-nav.php'; ?>
+
+    <section id="main-content">
+      <div class="container">
+
+        <article id="main-col">
+          <h3>Super Secret About Page</h3>
+          <p>This is the best about page on the face of the earth</p>
+          <p>
+            Bob is so dumb that he ran in the middle of the street. He ran there because he saw a car, and he thinks that cars can’t kill him. Bob is stupid. When Bob went in the middle of the street he was killed by a truck that couldn’t slow down fast enough, so Bob was crushed. “That’s too bad,” said one of the people that was walking down the street, but there was another person that liked Bob that ran in the middle of the street to drag the crushed Bob back to the side walk, but another truck came and crushed Sue too. “That’s the end of then for Shure,” said someone on the sidewalk. Then the truck driver finely came and said, “Someone’s ought to clean up this mess,” so the truck driver drug the crushed people to the sidewalk, then a hour later they woke up and said, “What happened?” The people that saw him were so surprised that they all fainted except one the one that didn’t faint told a lot of people and those people told other people until all the people in the world knew what happened; and after exactly one minute all the people of the world fainted. Bob and Sue looked around and saw that all the people in the world fainted and they said, “NO NO NO NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!!!!!!!!” and all their guts came out of their mouth and Bob and Sue fell in a little puddle of goo.
+          </p>
+        </article>
+
+        <aside>
+          <div class="dark">
+            <h3>We are trash</h3>
+            <p>Do not let us through out the trash or we will kil everyone. asldasdfasdasd asdfa.sdfasdfasd.fa  das sdf as as dasdasd sdf asdasdfa sdasd.fasd asdfjsdlsdkfajd sd alsdasdjlksdfjeioeafdkznf dfa sdfs. d fsfeo aseoaf sdl;aksdf e as.</p>
+          </div>
+        </aside>
+
+      </div>
+    </section>
+
+    <?php include 'inc/footer.php'; ?>
+
+  </body>
+</html>
