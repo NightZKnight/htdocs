63dd249a4240a4ca83f2126825d903aa401836c5
Timothy <32756638+NightZKnight@users.noreply.github.com>
2017-10-30 13:08:05 -0700

    updated about page

--- about.php
+++ about.php
@@ -15,9 +15,18 @@
         <article id="main-col">
           <h3>Super Secret About Page</h3>
           <p>This is the best about page on the face of the earth</p>
-          <p>
-            Bob is so dumb that he ran in the middle of the street. He ran there because he saw a car, and he thinks that cars can’t kill him. Bob is stupid. When Bob went in the middle of the street he was killed by a truck that couldn’t slow down fast enough, so Bob was crushed. “That’s too bad,” said one of the people that was walking down the street, but there was another person that liked Bob that ran in the middle of the street to drag the crushed Bob back to the side walk, but another truck came and crushed Sue too. “That’s the end of then for Shure,” said someone on the sidewalk. Then the truck driver finely came and said, “Someone’s ought to clean up this mess,” so the truck driver drug the crushed people to the sidewalk, then a hour later they woke up and said, “What happened?” The people that saw him were so surprised that they all fainted except one the one that didn’t faint told a lot of people and those people told other people until all the people in the world knew what happened; and after exactly one minute all the people of the world fainted. Bob and Sue looked around and saw that all the people in the world fainted and they said, “NO NO NO NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!!!!!!!!” and all their guts came out of their mouth and Bob and Sue fell in a little puddle of goo.
-          </p>
+          <div id="about-cards">
+            <h3>Better</h3>
+            <p>We make your stuff alot better <span style="font-size: 6pt;">don't ask</span></p>
+          </div>
+          <div id="about-cards">
+            <h3>Home improvment</h3>
+            <p>We don't really do this but, why would you think that anywhy?</p>
+          </div>
+          <div id="about-cards">
+            <h3>Cleanup crew</h3>
+            <p>We totally don’t do this</p>
+          </div>
         </article>
 
         <aside>
