f4a0190083fa7f6d6517b2824365375895128edb
Timothy <32756638+NightZKnight@users.noreply.github.com>
2017-11-06 12:41:15 -0800

    added LazyLoad.js and fixed form submiting
    form was still redirecting to the rc4.php when ajax was supposed to be
    used

--- test/index.php
+++ test/index.php
@@ -9,11 +9,21 @@
     <script src="../jquery-3.2.1.min.js" charset="utf-8"></script>
     <script type="text/javascript">
 
+
+      $(document).ready(function() {
+
+        $("#test").on("submit", function(e) {
+          e.preventDefault();
+        });
+
+      });
+
       function test(){
         // ajax the rc4.php and reset the password input box
         $.post( "rc4.php", $("#test").serialize(), function(data){
           $("#hi").html(data);
           $("form input[type=password]").val("");
+
         });
 
       }
@@ -25,9 +35,9 @@
     <div class="container">
 
      <form id="test" class="form" action="rc4.php" method="post">
-       <input type="text" name="name" value="files/cstest1.cs1">
+       <input type="text" name="name" value="files/CSTEST1.CS1">
        <input type="password" name="key" value="" placeholder="password">
-       <input type="button" name="Submit" value="Submit" onclick="test()"> <br><br>
+       <input type="submit" value="Submit" onclick="test()"> <br><br>
      </form>
      <div id="hi"></div>
 
