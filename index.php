59417a5b819693d9b7dfcd8b7135bb00f3961c66
Timothy <32756638+NightZKnight@users.noreply.github.com>
2019-01-26 01:27:00 -0800

    Got nav bar a little better now still needs some changes

--- index.php
+++ index.php
@@ -1,64 +1,73 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="master.css">
-    <script src="main.js" charset="utf-8"></script>
-    <link href="https://fonts.googleapis.com/css?family=Catamaran:500" rel="stylesheet">
-    <title>Cards</title>
-  </head>
-  <body>
+   <head>
+      <meta charset="utf-8">
+      <link rel="stylesheet" href="master.css">
+      <script src="main.js" charset="utf-8"></script>
+      <link href="https://fonts.googleapis.com/css?family=Catamaran:500" rel="stylesheet">
+      <title>Cards</title>
+   </head>
+   <body>
 
-    <!-- TODO stuff todo
-      Use flex box for the layout and grid-template-area for the infore mations
-      Autocomplete-paths
-      Fetch API MDN
-      Use transform for dropdown https://developer.mozilla.org/en-US/docs/Web/CSS/transform
-                                 https://discordapp.com/
-      Remove login avatar dummy and replace with hidden login button
-      Read VueJs Docs
-
-    -->
-
-    <div class="top-bar">
-      <img src="img/text.svg" alt="logo-text">
-
-      <input class="search-box" type="text" name="search" value="" placeholder="Search">
-
-      <img src="img/cookie-admin.jpg" alt="admin-pic">
-      <svg class="carrot" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><g><path fill="inherit" d="M14.1711599,9.3535 L9.99925636,13.529 L5.82735283,9.3535 C5.51262415,9.0385 5.73543207,8.5 6.18054835,8.5 L13.8179644,8.5 C14.2630807,8.5 14.4858886,9.0385 14.1711599,9.3535"></path></g></svg>
-    </div>
+         <!-- TODO stuff todo
+         Use flex box for the layout and grid-template-area for the infore mations
+         Autocomplete-paths
+         Fetch API MDN
+         Use transform for dropdown https://developer.mozilla.org/en-US/docs/Web/CSS/transform
+         https://discordapp.com/
+         Remove login avatar dummy and replace with hidden login button
+         Read VueJs Docs
+         https://codepen.io/simoberny/pen/WMMqwL
 
 
+         -->
 
-    <div class="wrapper">
-      <?php
+      <div class="top-bar">
+         <img class="flt-lt" src="img/text.svg" alt="logo-text">
 
-      $db = new PDO('sqlite:info.sqlite');
-      $sql = "SELECT * FROM articles";
+         <div class="avatar">
+            <img class="flt-rt" src="img/cookie-admin.jpg" alt="admin-pic">
+            <svg class="carrot" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><g><path fill="inherit" d="M14.1711599,9.3535 L9.99925636,13.529 L5.82735283,9.3535 C5.51262415,9.0385 5.73543207,8.5 6.18054835,8.5 L13.8179644,8.5 C14.2630807,8.5 14.4858886,9.0385 14.1711599,9.3535"></path></g></svg>
+         </div>
 
-      foreach ($db->query($sql) as $row) { //go thru each row in database and out put as a card.
+         <input class="search-box flt-rt" type="text" name="search" value="" placeholder="Search">
+      </div>
 
-       ?>
-
-       <a class="titleURL" href="<?php echo $row['url']; ?>" target="_blank">
-         <article class="card">
-           <div>
-             <?php echo $row['title']; ?>
-           </div>
-           <div class="dis">
-             <?php echo $row['dis']; ?>
-           </div>
-           <div class="picture">
-             <img src="<?php echo $row['img'] ?>" alt="og:image">
-           </div>
-         </article>
-       </a>
-
-      <?php } ?>
-    </div>
+      <div class="wrapper">
 
 
 
-  </body>
+
+
+
+         <div class="container">
+            <?php
+
+            $db = new PDO('sqlite:info.sqlite');
+            $sql = "SELECT * FROM articles";
+
+            foreach ($db->query($sql) as $row) { //go thru each row in database and out put as a card.
+
+               ?>
+
+               <a class="titleURL" href="<?php echo $row['url']; ?>" target="_blank">
+                  <article class="card">
+                     <div>
+                        <?php echo $row['title']; ?>
+                     </div>
+                     <div class="dis">
+                        <?php echo $row['dis']; ?>
+                     </div>
+                     <div class="picture">
+                        <img src="<?php echo $row['img'] ?>" alt="og:image">
+                     </div>
+                  </article>
+               </a>
+
+            <?php } ?>
+         </div>
+
+
+      </div>
+   </body>
 </html>
