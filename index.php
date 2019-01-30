3abf1dbf2576d8c054ec698e62f230879ac243e5
Timothy <32756638+NightZKnight@users.noreply.github.com>
2019-01-29 17:52:02 -0800

    Added dropdown took longer then it should have changed div.top-bar to <header> updated divs to html5 semantics elements added date row to info.sqlite updated gitignore

--- index.php
+++ index.php
@@ -22,31 +22,35 @@
 
          -->
 
-      <div class="top-bar">
+      <header>
          <img class="flt-lt" src="img/text.svg" alt="logo-text">
 
          <div class="avatar">
+            <div class="overlay"></div>
             <img class="flt-rt" src="img/cookie-admin.jpg" alt="admin-pic">
             <svg class="carrot" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><g><path fill="inherit" d="M14.1711599,9.3535 L9.99925636,13.529 L5.82735283,9.3535 C5.51262415,9.0385 5.73543207,8.5 6.18054835,8.5 L13.8179644,8.5 C14.2630807,8.5 14.4858886,9.0385 14.1711599,9.3535"></path></g></svg>
+            <ul class="hide">
+               <li> <a href="#">SIGN IN</a>  </li>
+               <li> <a href="#">LOG IN</a>  </li>
+               <li> <a href="#">HAVE</a> </li>
+            </ul>
          </div>
 
          <input class="search-box flt-rt" type="text" name="search" value="" placeholder="Search">
-      </div>
+      </header>
 
       <div class="wrapper">
 
 
 
 
-
-
-         <div class="container">
+         <section class="container">
             <?php
 
             $db = new PDO('sqlite:info.sqlite');
             $sql = "SELECT * FROM articles";
 
-            foreach ($db->query($sql) as $row) { //go thru each row in database and out put as a card.
+            foreach ($db->query($sql) as $row): //go thru each row in database and out put as a card.
 
                ?>
 
@@ -55,17 +59,22 @@
                      <div>
                         <?php echo $row['title']; ?>
                      </div>
+
+
+
                      <div class="dis">
                         <?php echo $row['dis']; ?>
                      </div>
+
                      <div class="picture">
-                        <img src="<?php echo $row['img'] ?>" alt="og:image">
+                        <img src="<?php echo $row['img']; ?>" alt="og:image">
                      </div>
+
                   </article>
                </a>
 
-            <?php } ?>
-         </div>
+            <?php endforeach; ?>
+         </section>
 
 
       </div>
--- main.js
+++ main.js
@@ -1,28 +1,52 @@
 window.onload = function() {
 
-  function Real(name1, age1) {
-    this.name = name1;
-    this.age = age1;
-    this.hair = 'brown';
-  }
+   function Real(name1, age1) {
+      this.name = name1;
+      this.age = age1;
+      this.hair = 'brown';
+   }
 
-  var ab = new Real('bob', 27);
-  console.log(ab.age);
+   var ab = new Real('bob', 27);
+   console.log(ab.age);
 
-  function $(id) {
-    return document.getElementById(id);
-  }
+   function $(id) {
+      return document.getElementById(id);
+   }
 
-  //onClick of the add button
-  //taggle class see-now on the dim element
-  //and hidden on the login element.
-  // $('add').onclick = function() {
-  //
-  //     $('dim').classList.toggle('see-now');
-  //     $('login-popup').classList.toggle('hidden');
-  //     scroll.top;
-  //
-  // }
+   //onClick of the add button
+   //taggle class see-now on the dim element
+   //and hidden on the login element.
+   // $('add').onclick = function() {
+   //
+   //     $('dim').classList.toggle('see-now');
+   //     $('login-popup').classList.toggle('hidden');
+   //     scroll.top;
+   //
+   // }
+
+   var avatar = document.getElementsByClassName('overlay')[0];
+   var avatarUl = document.querySelector('.avatar ul');
+
+   // toggles hide/show when avitar is clicked
+   avatar.onclick = function(e) {
+      document.querySelector('.avatar ul').classList.toggle('hide');
+      document.querySelector('.avatar ul').classList.toggle('show');
+   }
+
+   // if user clicks away form dropdown this hides the dropdown
+   window.onclick = function(e) {
+
+      if (!e.target.parentElement.classList.contains('avatar')) {
+
+         if (avatarUl.classList.contains('show')) {
+            avatarUl.classList.toggle('hide');
+            avatarUl.classList.toggle('show');
+         }
+      }
+
+
+   }
+
 
 
 }
