9c02f70c8a668e41c48ed2830d4261e69b062bab
Timothy <32756638+NightZKnight@users.noreply.github.com>
2019-02-14 18:26:58 -0800

    Added crud model and removed tmp

---
+++ application/models/Crud.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Crud extends CI_Model {
+
+	function __construct()
+	{
+		$this->load->database();
+	}
+
+	public function get_articles()
+	{
+		$this->db->select('title, dis, url, img, timeadded');
+		$quary = $this->db->get('news');
+		return $quary->result_array();
+	}
+
+}
