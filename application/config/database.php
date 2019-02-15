ba81e659c7ee61f861dfcc92fefa0dc074d09b22
Timothy <32756638+NightZKnight@users.noreply.github.com>
2019-02-14 16:33:48 -0800

    Changed config added database name add time_helper to autoload added file

--- application/config/database.php
+++ application/config/database.php
@@ -74,11 +74,11 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> 'mysql:host=127.0.0.1;dbname=dailymaze',
+	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
-	'database' => '',
+	'database' => 'dailymaze',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
